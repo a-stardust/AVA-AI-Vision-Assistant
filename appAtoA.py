a62539d528152804a79fb0ecbadb42ffc24e238e
visionassitance <kish73342@gmail.com>
2024-03-14 23:53:37 +0530

    cleaned code

--- appAtoA.py
+++ appAtoA.py
@@ -13,60 +13,38 @@ from face_recognizer import recognize_faces
 from src.prompt import system
 import os
 import numpy as np
+from transformers import pipeline
+from dotenv import load_dotenv
+warnings.filterwarnings("ignore")
 
 engine = pyttsx3.init()
 engine.setProperty("rate", 150)
 engine.setProperty("voice", "english-us")
-
-from dotenv import load_dotenv
 load_dotenv()
-
-from transformers import pipeline
-
 transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base.en")
-
 extractor = TextExtractor(r'C:\Program Files\Tesseract-OCR\tesseract.exe')
 
-
-
-warnings.filterwarnings("ignore")
-
-
-
-
-
 client = OpenAI(
-    # This is the default and can be omitted
     api_key=os.environ.get("api"),
 )
-
-
 messages = [{"role": "system", "content":system}]
-
-
-
 def CustomChatGPT(audio):
-
     sr, y = audio
     y = y.astype(np.float32)
     y /= np.max(np.abs(y))
     user_input=transcriber({"sampling_rate": sr, "raw": y})["text"]
     print (user_input)
-
     if 'read' in str(user_input):
         print("reading")
         messages.append({"role": "user", "content": user_input})
         extracted_text = extractor.extract_text_from_image("frame.jpg")
         print(extracted_text)
         messages.append({"role": "system", "content": f'extracted text:{extracted_text}'})
-
         response = client.chat.completions.create(
         messages=messages,
         model="gpt-3.5-turbo")
-        
         ChatGPT_reply =response.choices[0].message.content
         messages.append({"role": "assistant", "content": ChatGPT_reply})
-
     else:
         messages.append({"role": "user", "content": user_input})
         response = client.chat.completions.create(
@@ -74,12 +52,9 @@ def CustomChatGPT(audio):
         model="gpt-3.5-turbo")
         ChatGPT_reply = response.choices[0].message.content
         messages.append({"role": "assistant", "content": ChatGPT_reply})
-
     engine.save_to_file(ChatGPT_reply, "response.mp3")
     engine.runAndWait()
-
     return "response.mp3"
-
 def object_detection():
     detector = YoloDetector()
     cap = cv2.VideoCapture(1)
@@ -99,46 +74,16 @@ def object_detection():
                 faces = recognize_faces("frame.jpg")
                 messages.append({"role": "system", "content": announcement +"persons detected:" + str(faces)})
                 print(faces)
-                # names=''
-                # if len(faces) > 1:
-                #         for i in range(0,len(faces)-1):
-                #             names+=faces[i]
-                #             names+=' and '
-                #         names+=faces[len(faces)-1]
-                #         names= 'say hi to' + names
-                #         engine.save_to_file(names, "person.mp3")
-                #         sleep(1)                    #LINE ADDED
-                #         engine.runAndWait()
-                #         playsound('person.mp3')                    
-
-                # elif len(faces)==1 :
-                #     names='say hi to' + str(faces[0])
-                #     engine.save_to_file(names, "person.mp3")
-                #     engine.runAndWait()
-                #     playsound('/person.mp3')                    
-            # Append announcement to conversation
-
             else:
                 messages.append({"role": "system", "content": announcement})
-
-        # Break loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q') or keyboard.is_pressed('q'):
-            break
-
-        sleep(5)  # Wait for 5 seconds before checking again
+        sleep(5)  
     cap.release()
     cv2.destroyAllWindows()
-
 # Start object detection in a separate thread
 object_detection_thread = threading.Thread(target=object_detection)
 object_detection_thread.daemon = True
 object_detection_thread.start()
-
-
 demo = gr.Interface(fn=CustomChatGPT, inputs=gr.Audio(sources="microphone"), outputs="audio")
-
-
-
 demo.launch(share=True)
 
 
--- face_recognizer.py
+++ face_recognizer.py
@@ -1,39 +1,22 @@
 import pickle
 from collections import Counter
 from pathlib import Path
-
 import face_recognition
-
 DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
-
 def recognize_faces(image_location: str, model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH) -> list:
-    """
-    Given an unknown image, get the locations and encodings of any faces and
-    compares them against the known encodings to find potential matches.
-    Returns a list of recognized faces.
-    """
     with encodings_location.open(mode="rb") as f:
         loaded_encodings = pickle.load(f)
-
     input_image = face_recognition.load_image_file(image_location)
-
     input_face_locations = face_recognition.face_locations(input_image, model=model)
     input_face_encodings = face_recognition.face_encodings(input_image, input_face_locations)
-
     recognized_faces = []
     for unknown_encoding in input_face_encodings:
         name = _recognize_face(unknown_encoding, loaded_encodings)
         if not name:
             name = "Unknown"
         recognized_faces.append(name)
-
     return recognized_faces
-
 def _recognize_face(unknown_encoding, loaded_encodings):
-    """
-    Given an unknown encoding and all known encodings, find the known
-    encoding with the most matches.
-    """
     boolean_matches = face_recognition.compare_faces(
         loaded_encodings["encodings"], unknown_encoding
     )
@@ -44,7 +27,6 @@ def _recognize_face(unknown_encoding, loaded_encodings):
     )
     if votes:
         return votes.most_common(1)[0][0]
-
 # Example usage in another script:
 # from face_recognizer import recognize_faces
 # recognized_faces = recognize_faces("image.jpg")
